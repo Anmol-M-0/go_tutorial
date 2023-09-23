10c8c8567c815b120895e3e7f95b643d023f9e02
anmol <amaniyar000@gmail.com>
2023-09-23 13:13:57 +0530

    Updated

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,4 @@
-docker build -f build/Dockerfile.build -t go_tutorial:latest .
+# docker build -f build/Dockerfile.build -t go_tutorial:latest .
+docker build -f build/Dockerfile.binaries -t go_tutorial:latest --output=bin --target=binaries .
 
 docker run go_tutorial:latest
